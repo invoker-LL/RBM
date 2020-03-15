d70011754939ade06d67107477e576a725e564f0
invoker-LL <17816858740@163.com>
2020-03-15 16:29:35 +0800

    combine RBM and LSTM to one model. use RBM weights to init NN first dense layer

--- RBM.py
+++ RBM.py
@@ -1,81 +1,76 @@
-#based on Hinton's MATLAB RBM code from <www.sciencemag.org/cgi/content/full/313/5786/504/DC1>
-'''
-This program trains Restricted Boltzmann Machine in which
-visible, binary, stochastic pixels are connected to
-hidden, tochastic real-valued feature detectors drawn from a unit
-variance Gaussian whose mean is determined by the input from
-the logistic visible units. Learning is done with 1-step Contrastive Divergence.
-'''
 import torch
-class RBM():
+import torch.nn as nn
+class RBM(nn.Module):
     def __init__(self):
+        super(RBM, self).__init__()
         self.epsilon_w = 0.1  # Learning rate for weights
         self.epsilon_v_b = 0.1  # Learning rate for biases of visible units
         self.epsilon_h_b = 0.1  # Learning rate for biases of hidden units
         self.weight_cost = 0.0002
         self.initial_momentum = 0.5
         self.final_momentum = 0.9
-    def Train(self, batchdata, maxepoch=10,numhid = 8):
-        (numcases, numdims, numbatches)=batchdata.shape
 
+    def RBMtrain(self, data_loader, maxepoch=5, numdims=28,numhid=32):
         # Initializing symmetric weights and biases.
-        vishid = 0.1 * torch.randn(numdims, numhid)
-        hidbiases = torch.zeros(1, numhid)
-        visbiases = torch.zeros(1, numdims)
+        v_h_w = 0.1 * torch.randn(numdims, numhid)
+        h_bias = torch.zeros(1, numhid)
+        v_bias = torch.zeros(1, numdims)
 
-        vishidinc = torch.zeros(numdims, numhid)
-        hidbiasinc = torch.zeros(1, numhid)
-        visbiasinc = torch.zeros(1, numdims)
-        batchposhidprobs = torch.zeros(numcases, numhid, numbatches)
+        v_h_w_inc = torch.zeros(numdims, numhid)
+        h_bias_inc = torch.zeros(1, numhid)
+        v_bias_inc = torch.zeros(1, numdims)
 
         for epoch in range(maxepoch):
-            print(1, 'epoch %d\r', epoch)
-            errsum = 0
-            for batch in range(numbatches):
-                print(1, 'epoch %d batch %d\r', epoch, batch)
-                # START POSITIVE PHASE
-                data = batchdata[:,:, batch]
-                temp_tensor1 = hidbiases.repeat(numcases, 1)
+            err_sum = 0
+            cnt = 0
+            for batchdata in data_loader:
+                data, labels = batchdata
+                temp_shape = data.shape
+                data = data.reshape([temp_shape[0], -1])
+
+                (batchsize, _) = data.shape
+                temp_tensor1 = h_bias.repeat(batchsize, 1)
                 temp_shape = temp_tensor1.shape
-                poshidprobs = torch.ones(temp_shape) / (1 + torch.exp(-torch.matmul(data, vishid) - temp_tensor1))
-                batchposhidprobs[:,:, batch]=poshidprobs
+                poshidprobs = torch.ones(temp_shape) / (
+                        1 + torch.exp(-torch.matmul(data, v_h_w) - temp_tensor1))
                 posprods = torch.matmul(data.transpose(0, 1), poshidprobs)
                 poshidact = torch.sum(poshidprobs, 0)
                 posvisact = torch.sum(data, 0)
 
                 # END OF POSITIVE PHASE
-                poshidstates = (poshidprobs > torch.rand(numcases, numhid)).float()
+                poshidstates = (poshidprobs > torch.rand(batchsize, numhid)).float()
                 # START NEGATIVE PHASE
-                temp_tensor2 = visbiases.repeat(numcases, 1)
+                temp_tensor2 = v_bias.repeat(batchsize, 1)
                 temp_shape = temp_tensor2.shape
-                negdata = torch.ones(temp_shape) / (1 + torch.exp(-torch.matmul(poshidstates, vishid.transpose(0,1))
-                                                                  - temp_tensor2))
+                negdata = torch.ones(temp_shape) / (
+                        1 + torch.exp(-torch.matmul(poshidstates, v_h_w.transpose(0, 1))
+                                      - temp_tensor2))
                 temp_shape = temp_tensor1.shape
-                neghidprobs = torch.ones(temp_shape) / (1 + torch.exp(-torch.matmul(negdata, vishid) - temp_tensor1))
-                negprods = torch.matmul(negdata.transpose(0,1), neghidprobs)
-                neghidact = torch.sum(neghidprobs,0)
-                negvisact = torch.sum(negdata,0)
+                neghidprobs = torch.ones(temp_shape) / (
+                        1 + torch.exp(-torch.matmul(negdata, v_h_w) - temp_tensor1))
+                negprods = torch.matmul(negdata.transpose(0, 1), neghidprobs)
+                neghidact = torch.sum(neghidprobs, 0)
+                negvisact = torch.sum(negdata, 0)
 
                 # END OF NEGATIVE PHASE
-                err = sum(sum((data - negdata).pow(2)))
-                errsum += err
-
+                err = sum(sum((abs(data - negdata))))/batchsize
+                cnt += 1
+                err_sum += err
                 if epoch > 5:
                     momentum = self.final_momentum
                 else:
                     momentum = self.initial_momentum
 
                 # UPDATE WEIGHTS AND BIASES
-                vishidinc = momentum * vishidinc + \
-                            self.epsilon_w * ((posprods - negprods) / numcases - self.weight_cost * vishid)
-                visbiasinc = momentum * visbiasinc + (self.epsilon_v_b / numcases) * (posvisact - negvisact)
-                hidbiasinc = momentum * hidbiasinc + (self.epsilon_h_b / numcases) * (poshidact - neghidact)
+                v_h_w_inc = momentum * v_h_w_inc + \
+                            self.epsilon_w * ((posprods - negprods) / batchsize - self.weight_cost * v_h_w)
+                v_bias_inc = momentum * v_bias_inc + (self.epsilon_v_b / batchsize) * (posvisact - negvisact)
+                h_bias_inc = momentum * h_bias_inc + (self.epsilon_h_b / batchsize) * (poshidact - neghidact)
 
-                vishid = vishid + vishidinc
-                visbiases = visbiases + visbiasinc
-                hidbiases = hidbiases + hidbiasinc
+                v_h_w = v_h_w + v_h_w_inc
+                v_bias = v_bias + v_bias_inc
+                h_bias = h_bias + h_bias_inc
 
-                # END OF UPDATES
+            print('epoch: %d, error %.1f ' % (epoch, err_sum/cnt))
 
-                print(1, 'epoch %4i error %6.1f  \n', epoch, errsum)
-        return
+        return v_h_w, h_bias
--- RBMs-LSTM.py
+++ RBMs-LSTM.py
@@ -7,25 +7,34 @@ import torchvision.transforms as transforms
 import RBM
 import torch.nn as nn
 class RBMLSTMModel(nn.Module):
-    def __init__(self, ninput, n_rbm_unit, nhid, nlayers, noutput, dropout=0.25):
+    def __init__(self, ninput, n_rbm_unit, nhid, nlayers, noutput, dropout=0.25, RBM_weights = []):
         super(RBMLSTMModel, self).__init__()
         self.drop = nn.Dropout(dropout)
-        self.encoder = nn.Linear(ninput, n_rbm_unit)
+        self.encoder = nn.Linear(ninput*ninput, ninput * n_rbm_unit)
         self.rnn = nn.LSTM(n_rbm_unit, nhid, nlayers, dropout=dropout, batch_first=True)
         self.decoder = nn.Linear(nhid, noutput)
 
-        self.init_weights()
-        self.nhid = nhid
+        self.ninput = ninput
+        self.n_rbm_unit = n_rbm_unit
         self.nlayers = nlayers
+        self.nhid = nhid
 
-    def init_weights(self):
+        self.init_weights(RBM_weights)
+
+    def init_weights(self,RBM_weights):
         initrange = 0.1
-        self.encoder.weight.data.uniform_(-initrange, initrange) # todo,weights_from_rbm
         self.decoder.bias.data.zero_()
         self.decoder.weight.data.uniform_(-initrange, initrange)
+        if RBM_weights == []: # 1. default param init
+            self.encoder.weight.data.uniform_(-initrange, initrange)
+            self.encoder.bias.data.zero_()
+        else: # 2. init param with RBM weights!
+            self.encoder.weight.data = RBM_weights[0]
+            self.encoder.bias.data = RBM_weights[1]
 
     def forward(self, input, hidden):
-        z = self.drop(self.encoder(input))
+        z = self.encoder(input.reshape(-1, self.ninput * self.n_rbm_unit))
+        z = z.reshape(-1, self.ninput, self.n_rbm_unit)
         output, hidden = self.rnn(z, hidden)
         output = self.drop(output)
         decoded = self.decoder(output)
@@ -38,29 +47,86 @@ class RBMLSTMModel(nn.Module):
                 weight.new_zeros(self.nlayers, bsz, self.nhid))
         return (weight.new_zeros(self.nlayers, bsz, self.nhid).cuda(),
                 weight.new_zeros(self.nlayers, bsz, self.nhid).cuda())
+
+def train(model,trainloader,optimizer,device,criterion):
+    for i, data in enumerate(trainloader, 0):  # get the inputs; data is a list of [inputs, labels]
+        images, labels = data
+        temp_shape = images.shape
+        images = images.reshape([temp_shape[0], temp_shape[2], temp_shape[3]])
+        images = images.to(device)
+        labels = labels.to(device)
+        hidden = model.init_hidden(temp_shape[0], cuda_flag=True)
+        model.to(device)
+
+        model.zero_grad()
+        output = model(images, hidden)
+        loss = criterion(output[:, -1, :], labels)
+        loss.backward()
+        optimizer.step()
+
+    print('loss:', loss.item())
+
+def test(model,testloader,device,classes):
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            temp_shape = images.shape
+            hidden = model.init_hidden(temp_shape[0], cuda_flag=True)
+            images = images.reshape([temp_shape[0], temp_shape[2], temp_shape[3]])
+            images = images.to(device)
+            labels = labels.to(device)
+            model.to(device)
+            outputs = model(images, hidden)
+            _, predicted = torch.max(outputs[:, -1, :], 1)
+            c = (predicted == labels).squeeze()
+            for i in range(4):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+    sum_acc = 0
+    eps = 0.00000001
+    for i in range(10):
+        print('Accuracy of %3s : %2d %%' % (
+            classes[i], 100 * class_correct[i] / (class_total[i] + eps)))
+        sum_acc += class_correct[i] / (class_total[i] + eps) * 100
+
+    print('Accuracy of all: %.2f %%' % (sum_acc / 10))
+
 def main():
     # train on the GPU or on the CPU, if a GPU is not available
-    batch_size = 16
+    batch_size = 64
     transform = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Normalize((0.1307,), (0.3081,))])
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    print(device)
+
     # get MNIST data, and shuffle them
     trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    indices = torch.randperm(len(trainset)).tolist()
+    trainset = torch.utils.data.Subset(trainset, indices[:3000])
+
     testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+    indices = torch.randperm(len(testset)).tolist()
+    testset = torch.utils.data.Subset(testset, indices[:])
+
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+
     classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
-    #rbm = RBM.RBM()
-    #weights = rbm.Train(trainloader, numhid=28)
-    #use this weights to update first dense
+
+    # we will use RBM trained weights to init first encoder layer
+    #rbm = RBM.RBM().to(device)
+    #RBM_weights = rbm.RBMtrain(trainloader, numdims=28*28, numhid=28*28, maxepoch=2)
+
     model = RBMLSTMModel(
         noutput=10,
         ninput=28,
-        n_rbm_unit=32,
+        n_rbm_unit=28,
         nhid=64,
         nlayers=1,
+        RBM_weights=[]
     )
 
     # construct an optimizer
@@ -68,62 +134,16 @@ def main():
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(params, lr=0.005,
                                 momentum=0.9, weight_decay=0.0005)
-    def train(inputdata,labels):
-        # move model\data\hidden_states to the right device
-        hidden = model.init_hidden(batch_size, cuda_flag=True)
-        inputdata = inputdata.to(device)
-        labels = labels.to(device)
-        model.to(device)
 
-        model.zero_grad()
-        output = model(inputdata, hidden)
-        loss = criterion(output[:,-1,:], labels)
-        loss.backward()
-        optimizer.step()
-
-        return loss.item()
-    def test():
-        class_correct = list(0. for i in range(10))
-        class_total = list(0. for i in range(10))
-        with torch.no_grad():
-            for data in testloader:
-                hidden = model.init_hidden(batch_size, cuda_flag=True)
-                images, labels = data
-                temp_shape = images.shape
-                images = images.reshape([batch_size, temp_shape[2], temp_shape[3]])
-                images = images.to(device)
-                labels = labels.to(device)
-                model.to(device)
-                outputs = model(images,hidden)
-                _, predicted = torch.max(outputs[:,-1,:], 1)
-                c = (predicted == labels).squeeze()
-                for i in range(4):
-                    label = labels[i]
-                    class_correct[label] += c[i].item()
-                    class_total[label] += 1
-        sum_acc = 0
-        for i in range(10):
-            print('Accuracy of %3s : %2d %%' % (
-                classes[i], 100 * class_correct[i] / class_total[i]))
-            sum_acc += class_correct[i] / class_total[i] * 100
-
-        print('Accuracy of all: %.2f %%' % (sum_acc/10))
-
-    n_epoch = 5
+    n_epoch = 20
     for iter in range(n_epoch):
-        for i, data in enumerate(trainloader, 0): # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
-            temp_shape = inputs.shape
-            inputs = inputs.reshape([batch_size, temp_shape[2], temp_shape[3]])
-            loss = train(inputs, labels)
-            if i % 100 == 1:
-                print('loss:', loss)
-    print('Finished Training')
+        train(model,trainloader,optimizer,device,criterion)
 
     torch.save(model.state_dict(), 'lastParams')
-
     model.load_state_dict(torch.load('lastParams'))
+
     model.eval()
-    test()
+    test(model,testloader,device,classes)
+
 if __name__ == "__main__":
     main()
